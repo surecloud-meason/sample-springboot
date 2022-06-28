8d3cc068e03099f03fa481125d7305c0d58788b8
MathewEason <mathew.eason@surecloud.com>
2022-06-28 23:13:13 +0100

    MOAR tests and more POWAR

--- helloworld/src/test/java/com/surecloud/helloworld/HelloWorldApplication2Tests.java
+++ helloworld/src/test/java/com/surecloud/helloworld/HelloWorldApplication2Tests.java
@@ -33,6 +33,156 @@ class HelloWorldApplication2Tests
     }
 
     @Test
+    void contextLoadsFast5()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast6()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast7()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast8()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast9()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast10()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast11()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast12()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast13()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast14()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast15()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast16()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast17()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast18()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast19()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast20()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast21()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast22()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast23()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast24()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast25()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast26()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast27()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast28()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast29()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
     void contextLoadsSlow1()
         throws InterruptedException
     {
