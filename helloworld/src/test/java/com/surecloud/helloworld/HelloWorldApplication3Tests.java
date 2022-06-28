70e19e7369abde9ae7d7fd3e801de78ec42f9768
MathewEason <mathew.eason@surecloud.com>
2022-06-28 21:22:27 +0100

    MOAR tests

---
+++ helloworld/src/test/java/com/surecloud/helloworld/HelloWorldApplication3Tests.java
@@ -0,0 +1,42 @@
+package com.surecloud.helloworld;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplication3Tests
+{
+    @Test
+    void contextLoadsFast1()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast2()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast3()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsFast4()
+    {
+        assertThat( true ).isTrue();
+    }
+
+    @Test
+    void contextLoadsSlow1()
+        throws InterruptedException
+    {
+        Thread.sleep( 10000 );
+        assertThat( true ).isTrue();
+    }
+}
